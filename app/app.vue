c5aa712c6a490e72d1a942bf282e6c94b929dae6
Andrei Hudalla <46633092+paranoidPhantom@users.noreply.github.com>
2026-01-06 21:18:38 +0300

    Initial commit

---
+++ app/app.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+import { Button } from "primevue";
+
+import { useTheme } from "vue-tg";
+</script>
+
+<template>
+    <div>
+        <Button :label="'test'" />
+        <h1 class="font-bold text-5xl">L1</h1>
+    </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    compatibilityDate: "2025-07-15",
+    devtools: { enabled: true },
+    modules: ["@nuxt/eslint", "@nuxt/fonts"],
+    app: {
+        head: {
+            script: [{ src: "https://telegram.org/js/telegram-web-app.js" }],
+        },
+    },
+    css: ["@/assets/css/global.css"],
+    ssr: false,
+});
