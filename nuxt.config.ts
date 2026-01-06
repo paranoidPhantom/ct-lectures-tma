718e98e51b2d0fbc3f5b347f6b98b2fd96cc8027
Andrei Hudalla <46633092+paranoidPhantom@users.noreply.github.com>
2026-01-06 23:05:39 +0300

    feat: add tailwind and eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
     compatibilityDate: "2025-07-15",
     devtools: { enabled: true },
-    modules: ["@nuxt/eslint", "@nuxt/fonts"],
+    modules: ["@nuxt/eslint", "@nuxt/fonts", "@nuxtjs/tailwindcss"],
     app: {
         head: {
             script: [{ src: "https://telegram.org/js/telegram-web-app.js" }],
