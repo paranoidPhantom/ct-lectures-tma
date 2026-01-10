871ce23ac49385ee6b9e850f715ef11cccb09cc7
Andrei Hudalla <46633092+paranoidPhantom@users.noreply.github.com>
2026-01-10 14:03:08 +0300

    chore: adapt root to no SSR architecture

--- app/app.vue
+++ app/app.vue
@@ -1,21 +1,13 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+useThemeProvider();
+</script>
 
 <template>
-    <ClientOnly>
-        <div>
-            <UtilInsetProvider>
-                <UtilThemeProvider />
-                <main class="p-4">
-                    <NuxtLayout>
-                        <NuxtPage />
-                    </NuxtLayout>
-                </main>
-            </UtilInsetProvider>
-        </div>
-        <template #fallback>
-            <div class="h-screen flex justify-center items-center text-3xl">
-                <Icon name="svg-spinners:ring-resize" />
-            </div>
-        </template>
-    </ClientOnly>
+    <UtilInsetProvider>
+        <main class="p-4">
+            <NuxtLayout>
+                <NuxtPage />
+            </NuxtLayout>
+        </main>
+    </UtilInsetProvider>
 </template>
