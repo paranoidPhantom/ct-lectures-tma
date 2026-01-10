c6be9b13f5a468738cd331874d00f7ec1b871ccb
Andrei Hudalla <46633092+paranoidPhantom@users.noreply.github.com>
2026-01-10 14:01:02 +0300

    chore: add vueuse module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,18 @@
 export default defineNuxtConfig({
     compatibilityDate: "2025-07-15",
     devtools: { enabled: true },
-    modules: ["@nuxt/eslint", "@nuxt/fonts", "@nuxtjs/tailwindcss", "@nuxt/icon"],
+    modules: [
+      "@nuxt/eslint",
+      "@nuxt/fonts",
+      "@nuxtjs/tailwindcss",
+      "@nuxt/icon",
+      "@vueuse/nuxt",
+    ],
+    fonts: {
+        defaults: {
+            weights: ["100", "400", "700", "800", "900"],
+        },
+    },
     app: {
         head: {
             script: [{ src: "https://telegram.org/js/telegram-web-app.js" }],
